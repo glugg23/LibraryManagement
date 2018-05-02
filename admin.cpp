f3000108acc56ba523aad8432b19c06247cb6d32
glugg23 <m.leonhardt424@gmail.com>
2018-05-02 19:19:45 +0100

    Hacked together a way to view all users

--- admin.cpp
+++ admin.cpp
@@ -2,6 +2,13 @@
 
 #include <iostream>
 
+#include <bsoncxx/json.hpp>
+
+#include "PRIVATE.h"
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_document;
+
 void createUser(User &user, mongocxx::database &db) {
     std::cout << "createUser" << std::endl;
 }
@@ -27,7 +34,33 @@ void deleteBook(User &user, mongocxx::database &db) {
 }
 
 void viewAllUsers(User &user, mongocxx::database &db) {
-    std::cout << "viewAllUsers" << std::endl;
+    //Find all users
+    auto cursor = db[USERS].find({});
+
+    //For all users in collection
+    for(const bsoncxx::document::view &doc : cursor) {
+        //Find array of all their borrowed books
+        auto ids = doc["borrowedBooks"].get_array().value;
+
+        //Print their username
+        std::cout << "Username: " << doc["username"].get_utf8().value << std::endl;
+
+        std::cout << "Loaned books:" << std::endl;
+
+        //For all book ids in borrowed book array
+        for(const bsoncxx::v_noabi::array::element &id : ids) {
+            //Find corresponding book document
+            auto result = db[BOOKS].find_one(make_document(kvp("_id", id.get_value())));
+
+            //Print title and author
+            std::cout << result->view()["title"].get_utf8().value
+                      << " by " << result->view()["author"].get_utf8().value << std::endl;
+
+            //std::cout << bsoncxx::to_json(*result) << std::endl;
+        }
+        //std::cout << bsoncxx::to_json(doc) << std::endl;
+        std::cout << std::endl;
+    }
 }
 
 void viewAllBooks(User &user, mongocxx::database &db) {
