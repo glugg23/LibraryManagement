fa81de508aac599f9fc70f877f616c9ec1be84e1
glugg23 <m.leonhardt424@gmail.com>
2018-05-08 14:12:44 +0100

    Added advanced search options

--- search.cpp
+++ search.cpp
@@ -110,5 +110,61 @@ void searchBookGenre(User &user, mongocxx::database &db) {
 }
 
 void searchBookAdvanced(User &user, mongocxx::database &db) {
+    //Flush buffer as we're now using getline rather than just cin
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
+    std::string input, title, author, genre;
+
+    std::cout << "Search by title? (y/n): ";
+    std::getline(std::cin, input);
+
+    if(input == "y") {
+        std::cout << "Enter title: ";
+        std::getline(std::cin, title);
+    }
+
+    std::cout << "Search by author? (y/n): ";
+    std::getline(std::cin, input);
+
+    if(input == "y") {
+        std::cout << "Enter author: ";
+        std::getline(std::cin, author);
+    }
+
+    std::cout << "Search by genre? (y/n): ";
+    std::getline(std::cin, input);
+
+    if(input == "y") {
+        std::cout << "Enter genre: ";
+        std::getline(std::cin, genre);
+    }
+
+    auto cursor = db[BOOKS].find(
+        make_document(
+            kvp("title", make_document(kvp("$regex", title), kvp("$options", "i"))),
+            kvp("author", make_document(kvp("$regex", author), kvp("$options", "i"))),
+            kvp("genre", make_document(kvp("$regex", genre), kvp("$options", "i")))
+        )
+    );
+
+    std::cout << std::endl;
+
+    //Shows error but still works
+    for(const bsoncxx::document::view &doc : cursor) {
+        //Check if the book has already been borrowed
+        auto result = db[USERS].find_one(make_document(kvp("_id", doc["borrowedBy"].get_value())));
+
+        std::cout << doc["title"].get_utf8().value
+                  << " by " << doc["author"].get_utf8().value
+                  << ", in genre " << doc["genre"].get_utf8().value;
+
+        if(result) {
+            std::cout << ", currently unavailable" << std::endl;
+
+        } else {
+            std::cout << std::endl;
+        }
+    }
+
+    std::cout << std::endl;
 }
